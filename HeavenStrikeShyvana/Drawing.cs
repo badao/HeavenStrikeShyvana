1eee7da27780a681bc7b25524f17239084f6e91d
badao <huy_vu626@yahoo.com>
2015-11-23 08:00:50 +0700

    Release Shyvana

---
+++ HeavenStrikeShyvana/Drawing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using LeagueSharp;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+using ItemData = LeagueSharp.Common.Data.ItemData;
+
+
+namespace HeavenStrikeShyvana
+{
+    using static Program;
+    public static class Drawing
+    {
+        public static void OnDraw_Draw()
+        {
+            if (DrawE)
+                Render.Circle.DrawCircle(Player.Position, E.Range, Color.Green);
+            if (DrawR)
+                Render.Circle.DrawCircle(Player.Position, R.Range, Color.Green);
+        }
+    }
+}
---
+++ HeavenStrikeShyvana/harass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using LeagueSharp;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+using ItemData = LeagueSharp.Common.Data.ItemData;
+
+namespace HeavenStrikeShyvana
+{
+    using static Program;
+    using static extension;
+    class harass
+    {
+        public static void UpdateHarass()
+        {
+            var targetE = TargetSelector.GetTarget(E.Range, TargetSelector.DamageType.Magical);
+            if (EHarass && E.IsReady() && targetE.IsValidTarget() && !targetE.IsZombie
+                && Orbwalking.CanMove(80))
+            {
+                E.Cast(targetE);
+            }
+        }
+        public static void AfterAttackHarass(AttackableUnit unit, AttackableUnit target)
+        {
+            if (!(target is Obj_AI_Hero))
+                return;
+            if (Q.IsReady())
+            {
+                Q.Cast();
+            }
+            else if (HasItem())
+            {
+                CastItem();
+            }
+        }
+        public static void BeforeAttackHarass(Orbwalking.BeforeAttackEventArgs args)
+        {
+            if (!(args.Target is Obj_AI_Hero))
+                return;
+            var target = args.Target as Obj_AI_Base;
+            var x = Prediction.GetPrediction(args.Target as Obj_AI_Base, Player.AttackCastDelay + Game.Ping / 1000 + 0.02f);
+            var chasetime = (Player.Position.To2D().Distance(x.UnitPosition.To2D())
+                - Player.BoundingRadius - target.BoundingRadius - Player.AttackRange)
+                / (Player.MoveSpeed - target.MoveSpeed);
+            if (Q.IsReady() && Player.Position.To2D().Distance(x.UnitPosition.To2D())
+                >= Player.BoundingRadius + Player.AttackRange + args.Target.BoundingRadius
+                && (chasetime >= Player.AttackDelay || chasetime < 0))
+            {
+                args.Process = false;
+                Q.Cast();
+            }
+        }
+    }
+}
